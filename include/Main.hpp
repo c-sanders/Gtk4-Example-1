ea71fc2841457f4657e224dfe91ee3f0438d31cd
c-sanders <craig.sanders.gimp@gmail.com>
2023-09-24 23:38:15 +1000

    Initial commit.

---
+++ include/Main.hpp
@@ -0,0 +1,17 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+
+#include <string>
+#include <iostream>
+
+#include <gtk/gtk.h>
+
+#include <SignalHandlers.hpp>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+
+#endif
